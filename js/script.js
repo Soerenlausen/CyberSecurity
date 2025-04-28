6c7b4feb8918dd6d7d9d22703c45332b0913cf95
Søren Lausen <soelau01@iba.dk>
2025-04-28 09:15:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const btns = document.querySelectorAll('.btn-group button');
+const choices = []; // Array til id på knappen, der er blevet klikket på
+const sections = document.querySelectorAll('section'); // fast variabel, som henter de forskellige sectioner(elementer) fra html'en udfra teksten 'section'
+
+// Function til at skift section frem og tilbage
+const checkAnswer = (e) => { // Funktion med en parameter (e)
+    sections.forEach(section => {
+        section.style.display = 'none';
+    })
+    choices.push(e.target.id) // gemmer id i choices
+    console.log(choices)
+    switch (e.target.id) { // Forholder sig til det klikkede elements id
+        case 'a1': document.querySelector('#q3').style.display = 'flex'; // Viser elementet med id="q3"
+        break;
+        case 'a2': document.querySelector('#q2').style.display = 'flex'; // Viser elementet med id="q2"
+        break;
+        case 'a3': document.querySelector('#e1').style.display = 'flex'; // Viser elementet med id="e1"
+        break;
+        case 'a2-1': document.querySelector('#e3').style.display = 'flex'; // Viser elementet med id="q2"
+        break;
+        case 'a2-2': document.querySelector('#e2').style.display = 'flex'; // Viser elementet med id="e1"
+        break;
+        case 'a1-1': document.querySelector('#e3').style.display = 'flex'; // Viser elementet med id="q2"
+        break;
+        case 'a1-2': document.querySelector('#e2').style.display = 'flex'; // Viser elementet med id="e1"
+        break;
+        default: console.log("Error"); // Viser error, hvis id ikke findes
+    }
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('click', checkAnswer);
+})
